1973b8ea5c85968c2d796ca83aef7913bfcbbbff
Tes3awy <11368432+Tes3awy@users.noreply.github.com>
2025-01-30 22:43:18 +0200

    Refactor code

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -9,13 +9,13 @@ async function checkServerStatus() {
         if (response.ok && response.status === 200) {
             const data = await response.json();
             console.log(`[${new Date().toLocaleTimeString()}] Server Status:`, data.status);
-            if (data.status === 'up' && !status.classList.contains('text-green')) {
+            if (data.code === 200 && !status.classList.contains('text-green')) {
                 status.textContent = 'Up';
                 status.classList.add('text-green');
                 status.classList.remove('text-red');
                 statusIndicator.classList.add('status-green');
                 statusIndicator.classList.remove('status-red');
-            } else if (data.status === 'down' && !status.classList.contains('text-red')) {
+            } else if (data.code === 500 && !status.classList.contains('text-red')) {
                 status.textContent = 'Down';
                 status.classList.add('text-red');
                 status.classList.remove('text-green');
